a6439b7e5e09d9f15ddc366d5e491166327490e8
Phil <wright.phillip@gmail.com>
2016-05-18 15:09:01 +0000

    Update word lists

--- language_detector/languages.py
+++ language_detector/languages.py
@@ -2,43 +2,55 @@
 """This file contains the definition of all languages supported by the program."""
 
 LANGUAGES = [
-    {
+    { # http://www.vistawide.com/spanish/top_100_spanish_words.htm
         'name': 'Spanish',
         'common_words': [
-            'el', 'la', 'de', 'que', 'y', 'a', 'en', 'un', 'ser', 'se',
-            'no', 'haber', 'por', 'con', 'su', 'para', 'como', 'estar',
-            'tener', 'le', 'lo', 'lo', 'todo', 'pero', 'más', 'hacer',
-            'o', 'poder', 'decir', 'este', 'ir', 'otro', 'ese', 'la',
-            'si', 'me', 'ya', 'ver', 'porque', 'dar', 'cuando', 'él',
-            'muy', 'sin', 'vez', 'mucho', 'saber', 'qué', 'sobre',
-            'mi', 'alguno', 'mismo', 'yo', 'también', 'hasta'
+            'el', 'la', 'de', 'que', 'y', 'a', 'en', 'un ', 'ser ', 'se', 'no',
+            'haber', 'por', 'con', 'su', 'para', 'como', 'estar ', 'tener',
+            'le', 'lo', 'todo', 'pero', 'más', 'hacer', 'o', 'poder', 'decir',
+            'este', 'esta', 'ir', 'otro', 'ese', 'esa', 'si', 'me', 'ya',
+            'ver', 'porque', 'dar', 'cuando', 'él', 'muy', 'sin', 'vez',
+            'mucho', 'saber ', 'qué', 'sobre', 'mi', 'alguno ', 'mismo ', 'yo',
+            'también', 'hasta', 'año', 'dos', 'querer', 'entre', 'así',
+            'primero', 'desde', 'grande', 'eso', 'ni', 'nos', 'llegar',
+            'pasar', 'tiempo', 'ella', 'ellas', 'sí', 'día', 'uno', 'bien',
+            'poco ', 'deber', 'entonces', 'poner', 'cosa', 'tanto', 'hombre',
+            'parecer', 'nuestro', 'tan', 'donde', 'ahora', 'parte ', 'después',
+            'vida', 'quedar', 'siempre', 'creer', 'hablar ', 'llevar',
+            'dejar ', 'nada', 'cada', 'seguir', 'menos', 'nuevo', 'encontrar'
         ]
     },
-    {
+    { # http://www.languagedaily.com/learn-german/vocabulary/common-german-words
         'name': 'German',
         'common_words': [
-            'das', 'ist', 'du', 'ich', 'nicht', 'die', 'es', 'und',
-            'der', 'was', 'wir', 'zu', 'ein', 'er', 'in', 'sie', 'mir',
-            'mit', 'ja', 'wie', 'den', 'auf', 'mich', 'dass', 'so',
-            'hier', 'eine', 'wenn', 'hat', 'all', 'sind', 'von',
-            'dich', 'war', 'haben', 'für', 'an', 'habe', 'da', 'nein',
-            'bin', 'noch', 'dir', 'uns', 'sich', 'nur',
-            'einen', 'kann', 'dem'
+            'das', 'ist', 'du', 'ich', 'nicht', 'die', 'es', 'und', 'sie',
+            'der', 'was', 'wir', 'zu', 'ein', 'er', 'in', 'mir', 'mit', 'ja',
+            'wie', 'den', 'auf', 'mich', 'dass', 'daß', 'so', 'hier', 'eine',
+            'wenn', 'hat', 'all', 'sind', 'von', 'dich', 'war', 'haben', 'für',
+            'an', 'habe', 'da', 'nein', 'bin', 'noch', 'dir', 'uns', 'sich',
+            'nur', 'einen', 'kann', 'dem', 'auch', 'schon', 'als', 'dann',
+            'ihn', 'mal', 'hast', 'sein', 'ihr', 'aus', 'um', 'aber', 'meine',
+            'wird', 'doch', 'mein', 'bist', 'im', 'keine', 'gut', 'oder',
+            'weiß', 'jetzt', 'man', 'nach', 'werden', 'wo', 'oh', 'will',
+            'also', 'mehr', 'immer', 'muss', 'warum', 'bei', 'etwas', 'nichts',
+            'bitte', 'wieder', 'machen', 'diese', 'vor', 'können', 'hab',
+            'zum', 'gehen', 'sehr', 'geht', 'sehen'
         ]
     },
-    {
+    { # https://en.wikipedia.org/wiki/Most_common_words_in_English
         'name': 'English',
-        'common_words': ['the', 'be', 'to', 'of', 'and', 'a', 'in', 'that', 
-            'have', 'I', 'it', 'for', 'not', 'on', 'with', 'he', 'as', 'you', 
-            'do', 'at', 'this', 'but', 'his', 'by', 'from', 'they', 'we', 'say', 
-            'her', 'she', 'or', 'an', 'will', 'my', 'one', 'all', 'would', 'there', 
-            'their', 'what', 'so', 'up', 'out', 'if', 'about', 'who', 'get', 'which', 
-            'go', 'me', 'when', 'make', 'can', 'like', 'time', 'no', 'just', 'him', 
-            'know', 'take', 'people', 'into', 'year', 'your', 'good', 'some', 'could', 
-            'them', 'see', 'other', 'than', 'then', 'now', 'look', 'only', 'come', 'its', 
-            'over', 'think', 'also', 'back', 'after', 'use', 'two', 'how', 'our', 'work', 
-            'first', 'well', 'way', 'even', 'new', 'want', 'because', 'any', 'these', 'give', 
-            'day', 'most', 'us'
+        'common_words': ['the', 'be', 'to', 'of', 'and', 'a', 'in', 'that',
+            'have', 'I', 'it', 'for', 'not', 'on', 'with', 'he', 'as', 'you',
+            'do', 'at', 'this', 'but', 'his', 'by', 'from', 'they', 'we', 
+            'say', 'her', 'she', 'or', 'an', 'will', 'my', 'one', 'all',
+            'would', 'there', 'their', 'what', 'so', 'up', 'out', 'if',
+            'about', 'who', 'get', 'which', 'go', 'me', 'when', 'make', 'can',
+            'like', 'time', 'no', 'just', 'him', 'know', 'take', 'people',
+            'into', 'year', 'your', 'good', 'some', 'could', 'them', 'see',
+            'other', 'than', 'then', 'now', 'look', 'only', 'come', 'its',
+            'over', 'think', 'also', 'back', 'after', 'use', 'two', 'how',
+            'our', 'work', 'first', 'well', 'way', 'even', 'new', 'want',
+            'because', 'any', 'these', 'give', 'day', 'most', 'us'
         ]
     }
 ]
